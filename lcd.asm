99a2eb6f1e4406d86009da70b1b63f398b8f41e9
Andrew Davidson <andrewdavidson2005@gmail.com>
2020-09-19 23:19:16 +0100

    lcd now working!

--- lcd.asm
+++ lcd.asm
@@ -6,37 +6,70 @@ portb   equ     05h
 portc   equ     06h
 crtl    equ     07h
 
+;initialise LCD
+;set LCD to 8 bit operation
+;increment
+;cursor on and display on
+;clear display
+;display string
+
 initlcd ld      a,80h
         out     crtl,a
-        ld      a,0
+        sub     a
         out     porta,a
         out     portc,a
         
-viiibit ld      b,4
-loop     ld      a,38h
+lcdmode ld      a,38h
         out     porta, a
-        ld      a,80h
-        out     portc, a
-        sub     a
-        out     portc, a
-        djnz    loop
+        call    pulse
+       
 
-inc     ld      a,6
+increm  ld      a,6
         out     porta, a
-        ld      a,80h
-        out     portc,a
-        sub     a
-        out     portc,a
+        call    pulse
 
 dispon  ld      a,0ch
         out     porta,a
-        ld      a,80h
-        out     portc,80h
-        sub     a
+        call    pulse
+
+cls     ld      a,1
+        out     porta,a
+        call    pulse 
+        
+string  ld      hl,phrase
+print   ld      a,(hl)
+        cp      0
+        jp      z,finish
+        out     porta,a
+        ld      a,0a0h
         out     portc,a
         
+        push    af,bc,de,hl
+        call    delay1
+        pop     hl,de,bc,af
+        sub     a
+        out     portc,a
+        inc     hl
+        jp      print
 
+finish  ret
+
+pulse   ld      a,80h
+        out     portc, a
+       
+        push    af,bc,de,hl
+        call    delay1
+        pop     hl,de,bc,af
+        sub     a
+        out     portc, a
         ret
+
+delay1  ld      bc,4a38h
+de1     cpi
+        nop
+        nop
+        ret     po
+        jr      de1 
         
-phrase  db      "Hello, World", 0
+phrase  db      "Hello, Alistair!", 0
         outend
